2f7285ddfbe9303d8e4762984a7e1d395a49879e
Zachary Kampf <>
2019-03-31 18:39:12 -0400

    added combo and multipliers to scoring system, also added fail mechanism

--- RhythmTapUniverse-master/Assets/Scripts/BeatScroller.cs
+++ RhythmTapUniverse-master/Assets/Scripts/BeatScroller.cs
@@ -6,7 +6,7 @@ public class BeatScroller : MonoBehaviour
 {
 
     public float beatTempo;
-    public bool failed;
+    public bool failed=false;
 
     public bool hasStarted;
     // Start is called before the first frame update
--- RhythmTapUniverse-master/Assets/Scripts/GameManager.cs
+++ RhythmTapUniverse-master/Assets/Scripts/GameManager.cs
@@ -16,17 +16,15 @@ public class GameManager : MonoBehaviour
     public int currentScore;
     public int scorePerNote = 100;
     public int currentMulti;
-    public int multiTracker;
-    public int failTracker=0;
+    public int combo = 0;
+    public int failTracker;
     public int[] multiThresholds;
+    public int multiTracker;
     public Text scoreText;
+    public Text comboText;
     public Text multiText;
-    public GameObject failText;
-    public GameObject winText;
-    public GameObject startText;
-    public AudioSource winAudio;
     public float timer;
-
+    private IEnumerator coroutine;
 
 
     // Start is called before the first frame update
@@ -34,7 +32,12 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
        scoreText.text = "Score: 0";
+
         currentMulti = 1;
+        multiText.text = "Multiplier: X" + currentMulti;
+        failTracker = 5;
+        coroutine = waitTwo(2.0f);
+
     }
 
     // Update is called once per frame
@@ -47,42 +50,53 @@ public class GameManager : MonoBehaviour
             theBS.hasStarted = true;
             theMusic.Play();
             distMusic.Play();
-                //startText.SetActive(false);
+
 
 
             }
         }
 
-       if (startPlaying)
+       if (failTracker == 0)
+        {
+            theBS.failed = true;
+
+            StartCoroutine(coroutine);
+         
+        }
+
+        if (startPlaying)
         {
             timer += Time.deltaTime;
         }
 
-        /*if (failTracker >= 5)
-        {
-          
-            theBS.failed = true;
-            theMusic.pitch = 0.75f;
-            //failText.SetActive(true);
-            StartCoroutine("waitTwo");
-            startPlaying = false;
-        }
+        
 
-       if ((failTracker < 5) && (timer >= 45))
-        {
-            winText.SetActive(true);
-            winAudio.Play();
-            startPlaying = false;
-        }*/
+      
 
     }
     public void NoteHit()
     {
         Debug.Log("Hit On Time");
 
-            currentScore += scorePerNote * currentMulti;
+            combo++;
+            multiTracker++;
+        if (currentMulti - 1 < multiThresholds.Length)
+        {
+
+            if (multiThresholds[currentMulti - 1] <= multiTracker)
+            {
+                multiTracker = 0;
+                currentMulti++;
+
+            }
+        }
+        currentScore += scorePerNote * currentMulti;
             scoreText.text = "Score: " + currentScore;
-            distMusic.volume = 0;
+            comboText.text = "Combo: " + combo;
+            multiText.text = "Multiplier: X" + currentMulti;
+
+        distMusic.volume = 0;
+            
         }
 
 
@@ -91,16 +105,22 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("Missed Note");
         currentMulti = 1;
-        //multiTracker = 0;
-        //multiText.text = "Combo: x" + currentMulti;
-        failTracker++;
+        multiTracker = 0;
+
+        combo = 0;
+        failTracker--;
         distMusic.volume = 0.75f;
+        comboText.text = "Combo: " + combo;
+        multiText.text = "Multiplier: X" + currentMulti;
+
+
     }
 
-    IEnumerator waitTwo()
+    IEnumerator waitTwo(float waitTime)
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(waitTime);
         theMusic.Stop();
+        distMusic.Stop();
 
     }
 }
