20076752f9999327332647c38d6e7645848ff607
mugaverow <48916156+mugaverow@users.noreply.github.com>
2019-04-18 10:24:55 -0400

    Add files via upload

---
+++ AudioPeer.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samplesLeft = new float[512];
+    public static float[] _samplesRight = new float[512];
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    public static float[] _bufferDecrease = new float[8];
+
+    public float[] _freqBandHighest = new float[8];
+    public static float[] _audioBand = new float[8];
+    public static float[] _audioBandBuffer = new float[8];
+
+    public static float _Amplitude, _AmplitudeBuffer;
+    float _AmplitudeHighest;
+    public float _audioProfile;
+
+    public enum _channel { Stereo, Left, Right};
+    public _channel channel = new _channel();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        AudioProfile(_audioProfile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands ();
+        GetAmplitude();
+    }
+    void AudioProfile(float audioProfile)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            _freqBandHighest[i] = audioProfile;
+        }
+    }
+    void GetAmplitude()
+    {
+        float _CurrentAmplitude = 0;
+        float _CurrentAmplitudeBuffer = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            _CurrentAmplitude += _audioBand[i];
+            _CurrentAmplitudeBuffer += _audioBandBuffer[i];
+        }
+        if (_CurrentAmplitude > _AmplitudeHighest)
+        {
+            _AmplitudeHighest = _CurrentAmplitude;
+        }
+        _Amplitude = _CurrentAmplitude / _AmplitudeHighest;
+        _AmplitudeBuffer = _CurrentAmplitudeBuffer / _AmplitudeHighest;
+    }
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_freqBand [i] > _freqBandHighest [i])
+            {
+                _freqBandHighest[i] = _freqBand[i];
+            }
+            _audioBand[i] = (_freqBand[i] / _freqBandHighest[i]);
+            _audioBandBuffer[i] = (_bandBuffer[i] / _freqBandHighest[i]);
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData (_samplesLeft, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData (_samplesRight, 1, FFTWindow.Blackman);
+    }
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; ++g)
+        {
+            if (_freqBand [g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBand[g];
+                _bufferDecrease[g] = 0.005f;
+            }
+            if (_freqBand [g] < _bandBuffer[g])
+            {
+                _bandBuffer [g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+    void MakeFrequencyBands()
+    {
+        /*
+         * 22050 / 512 = 43herts per sample
+         * 0 - 2 = 86 hertz
+         * 1 - 4 = 172 - 87-258
+         * 2 - 8 = 344 - 259-602
+         * 3 - 16
+         * 4 - 32
+         * 5 - 64
+         * 6 - 128
+         * 7 - 256
+         */
+         
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) {
+
+
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow( 2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                if (channel == _channel.Stereo)
+                {
+                    average += _samplesLeft[count] + _samplesRight[count] * (count + 1);
+                }
+                if (channel == _channel.Left)
+                {
+                    average += _samplesLeft[count] * (count + 1);
+                }
+                if (channel == _channel.Right)
+                {
+                    average += _samplesRight[count] * (count + 1);
+                }
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
+    }
+}
